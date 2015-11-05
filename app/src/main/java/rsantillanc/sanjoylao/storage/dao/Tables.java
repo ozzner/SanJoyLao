f4ea388c41ac71d5582434049f4c42d31e0df7b0
Renzo D Santillan Ch <rsantillanc@gmail.com>
2015-11-05 18:10:04 -0500

    SJL database modify

--- app/src/main/java/rsantillanc/sanjoylao/storage/dao/Tables.java
+++ app/src/main/java/rsantillanc/sanjoylao/storage/dao/Tables.java
@@ -86,6 +86,124 @@ public class Tables {
             "\tPRIMARY KEY(objectId)\n" +
             ");";
 
+    public static final String CREATE_CALCULATION_TIME = "CREATE TABLE `CALCULATION_TIME` (\n" +
+            "\t`objectId`\tTEXT NOT NULL,\n" +
+            "\t`alteredTime`\tINTEGER DEFAULT 0,\n" +
+            "\t`calculatedTime`\tINTEGER DEFAULT 0,\n" +
+            "\t`idOrder`\tTEXT,\n" +
+            "\t`createdAt`\tTEXT,\n" +
+            "\t`updatedAt`\tTEXT,\n" +
+            "\tPRIMARY KEY(objectId)\n" +
+            ");";
+
+
+  public static final String CREATE_FEAST = "CREATE TABLE `FEAST` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`image`\tTEXT,\n" +
+          "\t`included`\tTEXT,\n" +
+          "\t`name`\tTEXT,\n" +
+          "\t`flagOption`\tINTEGER DEFAULT 0,\n" +
+          "\t`price`\tNUMERIC DEFAULT 0.0,\n" +
+          "\t`quantityPerson`\tINTEGER DEFAULT 0,\n" +
+          "\t`flagStatus`\tINTEGER DEFAULT 1,\n" +
+          "\t`createdAt`\tTEXT,\n" +
+          "\t`updatedAt`\tTEXT,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
+
+  public static final String CREATE_FEAST_PLATE = "CREATE TABLE `FEAST_PLATE` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`idFeast`\tTEXT,\n" +
+          "\t`idPlate`\tTEXT,\n" +
+          "\t`timeOfPreparation`\tINTEGER DEFAULT 0,\n" +
+          "\t`createdAt`\tTEXT,\n" +
+          "\t`updatedAt`\tTEXT,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
+
+  public static final String CREATE_LOCAL_RESTAURANT = "CREATE TABLE `LOCAL_RESTAURANT` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`Field2`\tINTEGER,\n" +
+          "\t`deliveriAvailable`\tINTEGER DEFAULT 0,\n" +
+          "\t`idRestaurant`\tTEXT,\n" +
+          "\t`location`\tTEXT,\n" +
+          "\t`reservationAvailable`\tINTEGER DEFAULT 0,\n" +
+          "\t`createdAt`\tTEXT,\n" +
+          "\t`updatedAt`\tTEXT,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
+
+  public static final String CREATE_LOCATION_DELIVERY = "CREATE TABLE `LOCATION_DELIVERY` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`location`\tTEXT,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
+
+  public static final String CREATE_ORDER_DETAIL = "CREATE TABLE `ORDER_DETAIL` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`idFeastPlate`\tTEXT,\n" +
+          "\t`idOrder`\tTEXT,\n" +
+          "\t`idPlateSize`\tTEXT,\n" +
+          "\t`createdAt`\tTEXT,\n" +
+          "\t`updatedAt`\tTEXT,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
+
+  public static final String CREATE_PAYMENT = "CREATE TABLE `PAYMENT` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`createdAt`\tTEXT,\n" +
+          "\t`updatedAt`\tTEXT,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
+
+  public static final String CREATE_RESTAURANT = "CREATE TABLE `RESTAURANT` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`about`\tTEXT,\n" +
+          "\t`centralCallNumber`\tINTEGER,\n" +
+          "\t`name`\tTEXT,\n" +
+          "\t`ruc`\tINTEGER,\n" +
+          "\t`slogan`\tTEXT,\n" +
+          "\t`createdAt`\tTEXT,\n" +
+          "\t`updatedAt`\tTEXT,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
+
+  public static final String CREATE_RUSH_HOUR = "CREATE TABLE `RUSH_HOUR` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`aditionalTime`\tINTEGER DEFAULT 0,\n" +
+          "\t`day`\tTEXT,\n" +
+          "\t`fromHour`\tTEXT DEFAULT '00:00',\n" +
+          "\t`toHour`\tTEXT DEFAULT '23:59',\n" +
+          "\t`createdAt`\tTEXT,\n" +
+          "\t`updatedAt`\tTEXT,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
+
+  public static final String CREATE_STATUS = "CREATE TABLE `STATUS` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`code`\tINTEGER DEFAULT 0,\n" +
+          "\t`name`\tINTEGER,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
+
+  public static final String CREATE_SUGGESTION = "CREATE TABLE `SUGGESTION` (\n" +
+          "\t`objectId`\tTEXT NOT NULL,\n" +
+          "\t`code`\tTEXT,\n" +
+          "\t`idFeastPlate`\tTEXT,\n" +
+          "\t`name`\tTEXT,\n" +
+          "\t`createdAt`\tTEXT,\n" +
+          "\tPRIMARY KEY(objectId)\n" +
+          ");";
+
 
 
     //Drop
@@ -99,4 +217,16 @@ public class Tables {
     public static final String PLATE = "PLATE";
     public static final String ORDER_TYPE = "ORDER_TYPE";
     public static final String ORDER = "ORDER";
+    public static final String ORDER_DETAIL = "ORDER_DETAIL";
+    public static final String CALCULATION_TIME = "CALCULATION_TIME";
+    public static final String FEAST = "FEAST";
+    public static final String FEAST_PLATE = "FEAST_PLATE";
+    public static final String LOCAL_RESTAURANT = "LOCAL_RESTAURANT";
+    public static final String LOCATION_DELIVERY = "LOCATION_DELIVERY";
+    public static final String PAYMENT = "PAYMENT";
+    public static final String RESTAURANT = "RESTAURANT";
+    public static final String RUSH_HOUR = "RUSH_HOUR";
+    public static final String STATUS = "STATUS";
+    public static final String SUGGESTION = "SUGGESTION";
+
 }
