b2f3b7c36801cb32751f2585b1d1c73181235c5e
rsantillan <rsantillan@intico.com.pe>
2015-12-03 08:25:23 -0500

    Time zone

--- app/src/main/java/rsantillanc/sanjoylao/util/SJLDates.java
+++ app/src/main/java/rsantillanc/sanjoylao/util/SJLDates.java
@@ -73,6 +73,14 @@ public class SJLDates {
         return strDate;
     }
 
+//    String dtc = "2014-04-02T07:59:02.111Z";
+//    SimpleDateFormat readDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+//    readDate.setTimeZone(TimeZone.getTimeZone("GMT")); // missing line
+//    Date date = readDate.parse(dtc);
+//    SimpleDateFormat writeDate = new SimpleDateFormat("dd.MM.yyyy, HH.mm");
+//    writeDate.setTimeZone(TimeZone.getTimeZone("GMT+04:00"));
+//    String s = writeDate.format(date);
+
 
     public static String customDateConverter(String Mydate, String MyformatIn, String MyformatOut) {
         String inputStringDate = Mydate;
